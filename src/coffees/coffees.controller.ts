07ac9046008e692a98ac50093135ee4e71a551e4
Pooya Golchian <pooya.golchian@gmail.com>
2024-04-29 20:17:11 +0400

    ✨ (coffees.controller.ts): add support for pagination in the findAll method to limit and offset the results ✨ (coffees.controller.ts): add new routes for updating and deleting a specific coffee by its id

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,18 +1,40 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-@Controller('/api/coffees')
+@Controller('coffees')
 export class CoffeesController {
-  @Get('/flavors')
-  findAll() {
-    return 'This action returns all the coffees!';
+  @Get()
+  findAll(@Query() paginationQuery): string {
+    const { limit, offset } = paginationQuery;
+    return `This action returns all coffees. Limit ${limit}, offset: ${offset}`;
   }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns ${id} coffees!`;
+    return `This action returns #${id} coffee`;
   }
 
   @Post()
   create(@Body() body) {
     return body;
+    // return `This action creates a coffee`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
   }
 }
